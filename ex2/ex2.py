336b5f71009da07d8bcf1a4418792215da35e133
itamar sharify <itamarsharify@gmail.com>
2016-04-06 17:19:23 +0300

    added basic graphs

--- ex2/ex2.py
+++ ex2/ex2.py
@@ -1,7 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x_inputs = [[1,0,0,1],[1,0,1,1],[1,1,0,1],[1,1,1,0]]
 r = 0.1
 z = 1
 t = 0.5
-
+i = 0
 w0 = 0
 w1 = 0
 w2 = 0
@@ -13,14 +17,13 @@ if __name__ == "__main__":
     w1_c = w1
     w2_c = w2
     while(True):
-
         w0_t = w0
         w1_t = w1
         w2_t = w2
-        x0 = int(raw_input('X0: '))
-        x1 = int(raw_input('X1: '))
-        x2 = int(raw_input('X2: '))
-        z = int(raw_input('z: '))
+        x0 = x_inputs[i][0]
+        x1 = x_inputs[i][1]
+        x2 = x_inputs[i][2]
+        z = x_inputs[i][3]
 
         c0 = w0*x0
         c1 = w1*x1
@@ -39,12 +42,22 @@ if __name__ == "__main__":
         print "X0 | X1 | X2 | Z | W0  | W1  | W2  | C0  | C1  | C2  |  S  |  D  | W0  | W1  | W2  |"
         print "%d  | %d  | %d  | %d | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f |" % (x0,x1,x2,z,w0_t,w1_t,w2_t,c0,c1,c2,s,d,w0,w1,w2)
         file.write("%d  | %d  | %d  | %d | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f |\n" % (x0,x1,x2,z,w0_t,w1_t,w2_t,c0,c1,c2,s,d,w0,w1,w2))
-        
+
         if w0_c == c0 and w1_c == c1 and w2_c == c2:
             count += 1
         if count == 2:
             file.close()
-            break        
+            break
         w0_c = c0
-        w1_c = c1 
+        w1_c = c1
         w2_c = c2
+        if i==3:
+            plt.axis([0,1,0,1])
+            x = np.arange(0, 1, 0.1)
+            if w == 0:
+                w = 0.1
+            y = x1+x2-x/w0
+            plt.plot(x, y)
+            plt.show()
+
+        i = (i + 1) % 4
