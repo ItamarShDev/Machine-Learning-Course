04588a13c216d6f9c6d5a4117018cc18e5896427
itamar sharify <itamarsharify@gmail.com>
2016-04-26 12:24:30 +0300

    added live plot!

--- ex2/ex2.py
+++ ex2/ex2.py
@@ -1,63 +1,65 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from pylab import *
+import copy
 
-x_inputs = [[1,0,0,1],[1,0,1,1],[1,1,0,1],[1,1,1,0]]
+def plot():
+    """Will show plot eveytime with the selected points. Uses global variables"""
+    # plt.title(string)
+    plt.xlabel('X1')
+    plt.ylabel('X2')
+    plt.plot([0, 0, 1, 1], [0, 1, 0, 1], 'ro', ms=10)
+    plt.axis([-1, 2, -1, 2])
+    plt.plot([1], [1], 'bo', ms=10)
+    plt.plot([-1, 2], Y)
+    plt.plot
+
+x_inputs = np.array([[1,0,0],[1,0,1],[1,1,0],[1,1,1]])
+z_outputs = np.array([[1],[1],[1],[0]])
 r = 0.1
 z = 1
 t = 0.5
 i = 0
-w0 = 0
-w1 = 0
-w2 = 0
+w = [0,0,0.05]
+old_w = [0,0,0]
 count = 0
 if __name__ == "__main__":
     file = open("results.txt", 'w+')
     file.write("X0|X1|X2|Z|W0|W1|W2|C0|C1|C2|S |D |W0|W1|W2|\n")
-    w0_c = w0
-    w1_c = w1
-    w2_c = w2
     while(True):
-        w0_t = w0
-        w1_t = w1
-        w2_t = w2
-        x0 = x_inputs[i][0]
-        x1 = x_inputs[i][1]
-        x2 = x_inputs[i][2]
-        z = x_inputs[i][3]
-
-        c0 = w0*x0
-        c1 = w1*x1
-        c2 = w2*x2
-        s = c0 + c1 + c2
+        c = x_inputs[i, 0:3] * w
+        s = np.sum(c)
         n = 0
         if s > t:
             n = 1
-        e = z - n
+        e = int(z_outputs[i] - n)
         d = r*e
-        w0 += x0 * d
-        w1 += x1 * d
-        w2 += x2 * d
-
+        old_w = copy.copy(w)
+        w += x_inputs[i, 0:3] * d
         print 100*"-"
         print "X0 | X1 | X2 | Z | W0  | W1  | W2  | C0  | C1  | C2  |  S  |  D  | W0  | W1  | W2  |"
-        print "%d  | %d  | %d  | %d | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f |" % (x0,x1,x2,z,w0_t,w1_t,w2_t,c0,c1,c2,s,d,w0,w1,w2)
-        file.write("%d  | %d  | %d  | %d | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f |\n" % (x0,x1,x2,z,w0_t,w1_t,w2_t,c0,c1,c2,s,d,w0,w1,w2))
+        print "%d  | %d  | %d  | %d | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f |" % (x_inputs[i, 0], x_inputs[i, 1], x_inputs[i, 2], z_outputs[i], old_w[0], old_w[1], old_w[2], c[0], c[1], c[2], s, d, w[0], w[1], w[2] )
+        file.write( "%d  | %d  | %d  | %d | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f | %.1f |\n" % (x_inputs[i, 0], x_inputs[i, 1], x_inputs[i, 2], z_outputs[i], old_w[0], old_w[1], old_w[2], c[0], c[1], c[2], s, d, w[0], w[1], w[2]))
+        y1 = (t-w[1]*(-1)-w[0])/w[2]
+        y2 = (t-2*w[1]-w[0])/w[2]
 
-        if w0_c == c0 and w1_c == c1 and w2_c == c2:
+        Y = [y1, y2]
+        plot()
+        pause(0.2)
+        graph = plt.figure()
+        plt.close(graph)
+        plt.close()
+        plt.show()
+
+
+        if (old_w == w).all():
             count += 1
-        if count == 2:
-            file.close()
-            break
-        w0_c = c0
-        w1_c = c1
-        w2_c = c2
-        if i==3:
-            plt.axis([0,1,0,1])
-            x = np.arange(0, 1, 0.1)
-            if w == 0:
-                w = 0.1
-            y = x1+x2-x/w0
-            plt.plot(x, y)
-            plt.show()
-
+            if count == 3:
+                plot()
+                plt.show()
+                file.close()
+                print "DONE"
+                break
+        else:
+            count = 0
         i = (i + 1) % 4
